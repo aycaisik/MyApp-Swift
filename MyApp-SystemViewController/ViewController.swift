4c30201eefd6b428b5bef0b7dda9705803a08115
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2023-01-11 23:54:44 +0300

    Added IBaction funcs.

--- MyApp-SystemViewController/ViewController.swift
+++ MyApp-SystemViewController/ViewController.swift
@@ -8,11 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - UI Elements
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    //MARK: - Properties
+    
+    //MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //MARK: - Functions
+    
+    //MARK: -Actions
+    
+    @IBAction func shareButtonTapped(_ button: UIButton){
+        
+        
+    }
+    @IBAction func safariButtonTapped(_ button: UIButton){
+        
+    }
+    @IBAction func photosButtonTapped(_ button: UIButton){
+        
+    }
+    @IBAction func emailButtonTapped(_ button: UIButton){
+        
+    }
+
 
 
 }
